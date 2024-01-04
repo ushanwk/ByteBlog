57b2400966ffe27f4483d6987de401d179a5a710
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-05 00:24:17 +0530

    nav bar created and post developing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {NavBar} from "./view/NavBar/NavBar";
+import car from './assets/images/car.jpeg'
 
 function App() {
   return (
-      <div>
+      <main className="w-full">
 
-      </div>
+           <header className="shadow-xl">
+               <div className="max-w-6xl h-16 mx-auto flex items-center justify-between px-10">
+                   <h1 className="font-sans font-bold text-2xl">ByteBlog.</h1>
+                   <nav className="flex gap-5">
+                       <a href="">Login</a>
+                       <a href="">Register</a>
+                   </nav>
+               </div>
+           </header>
+
+          <section className="max-w-6xl mx-auto py-24 px-10">
+            <div className="flex gap-6 max-md:flex-wrap items-center justify-center">
+                <img src={car} className="w-[40%] max-md:w-[60%]"/>
+                <div className="items-center justify-center flex flex-col gap-5">
+                    <h2 className="font-sans font-bold text-3xl">Unveiling the Future: Exploring the Latest Electric Car</h2>
+                    <p className="text-xl">Dive into the electric revolution! Explore cutting-edge technologies shaping the future of sustainable driving. From eco-friendly innovations to performance enhancements, discover the electric car's transformative journey towards a greener tomorrow.</p>
+                </div>
+            </div>
+          </section>
+
+      </main>
   );
 }
 
--- client/src/view/NavBar/NavBar.tsx
+++ client/src/view/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 export const NavBar = () => {
     return (
         <div>
-            <h1>Hello</h1>
+
         </div>
     );
 };
