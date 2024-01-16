5283dd2b96da8a42b5a5e35a797198d4c54c82d1
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-16 22:33:55 +0530

    mongo db connected and save a user function done

--- client/src/view/pages/RegisterPage.tsx
+++ client/src/view/pages/RegisterPage.tsx
@@ -1,12 +1,42 @@
+import {useState} from "react";
+
 export const RegisterPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(ev: any) {
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register', {
+          method: 'POST',
+          body: JSON.stringify({username, password}),
+          headers: {'Content-Type':'application/json'},
+        })
+    }
+
     return (
-        <form className="max-w-[500px] flex flex-col mx-auto items-center gap-5 mt-24 max-sm:mx-10">
+        <form onSubmit={register} className="max-w-[500px] flex flex-col mx-auto items-center gap-5 mt-24 max-sm:mx-10">
             <h1 className="text-center text-5xl font-bold font-sans mb-8">Register</h1>
-            <input type="text" placeholder="username"
-                   className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"/>
-            <input type="password" placeholder="password"
-                   className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"/>
-            <button className="w-64 bg-black text-white rounded-xl p-2 mt-4">Register</button>
+
+            <input
+                type="text" placeholder="username"
+                className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
+                value={username}
+                onChange={ev => setUsername(ev.target.value)}
+            />
+
+            <input
+                type="password" placeholder="password"
+                className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
+                value={password}
+                onChange={ev => setPassword(ev.target.value)}
+            />
+
+            <button
+                className="w-64 bg-black text-white rounded-xl p-2 mt-4"
+            >
+                Register
+            </button>
         </form>
     );
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
 
-app.get('/test', (req, res) => {
-    res.json('test ok')
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://ushanwk22:Ukwk22711@cluster0.x8nkhgc.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 })
 
 app.listen(4000);
