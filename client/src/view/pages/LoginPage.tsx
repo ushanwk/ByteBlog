77e1375c2ef210d1837a48d8902902c2f3173665
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-16 20:27:02 +0530

    register and login page front end done

--- client/src/view/pages/LoginPage.tsx
+++ client/src/view/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 export const LoginPage = () => {
     return (
-        <form className="max-w-[500px] flex flex-col mx-auto items-center gap-5 mt-24">
+        <form className="max-w-[500px] flex flex-col mx-auto items-center gap-5 mt-24 max-sm:mx-10">
             <h1 className="text-center text-5xl font-bold font-sans mb-8">Login</h1>
             <input type="text" placeholder="username"
                    className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"/>
