03880ef62648fbef8c1278336f8de66d9ac8e29d
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-17 21:38:58 +0530

    bcrypt add

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +16,7 @@ app.post('/register', async (req, res) => {
     const {username, password} = req.body;
 
     try{
-        const userDoc = await User.create({username, password});
+        const userDoc = await User.create({username, password: bcrypt.hashSync(password, salt),});
         res.json(userDoc);
     }catch (e){
         res.status(400).json(e)
