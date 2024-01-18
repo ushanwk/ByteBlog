162d78e1e1db11adf0950c17e1436a1397ec2d31
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-18 19:16:37 +0530

    create post fuctions developing

--- client/src/view/pages/CreatePost.tsx
+++ client/src/view/pages/CreatePost.tsx
@@ -1,27 +1,57 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css'
+import {useState} from "react";
+
+const modules = {
+    toolbar: [
+        [{'header': [1, 2, false]}],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image'],
+        ['clean']
+    ],
+}
+
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
 
 export const CreatePost = () => {
+
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+
     return (
         <form className="max-w-[700px] flex flex-col mx-auto items-center gap-5 mt-24 max-md:mx-10">
             <h1 className="text-center text-4xl font-bold font-sans mb-8">Create a Post</h1>
 
             <input
                 type="title" placeholder={'Title'}
+                value={title}
+                onChange={e => {setTitle(e.target.value)}}
                 className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
             />
 
             <input
                 type="summary" placeholder={'Summary'}
+                value={summary}
+                onChange={e => {setSummary(e.target.value)}}
                 className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
             />
 
             <input
-                type="file" placeholder={'Summary'}
+                type="file"
                 className="p-3 border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
             />
 
             <ReactQuill
+                value={content}
+                onChange={newValue => {setContent(newValue)}}
+                modules={modules} formats={formats}
                 className="border-2 border-gray-300 bg-gray-100 rounded-xl m-2 w-full"
             />
 
